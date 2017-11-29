5ba2813164030d3175b3d236c0a874b62611ed81
brankeye <brankeye@gmail.com>
2017-11-28 21:39:34 -0500

    Moved logic into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+    
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Failed to read file"); 
+    
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,33 +20,6 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-    
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Failed to read file"); 
-    
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
     }
 }
 
